a071cbef7320c011c23fc30c6ba5b226f577f3da
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-13 15:15:46 +0200

    exercise 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const query = { _id: req.todo._id };
+  const update = { $set: { text: req.body.text, done: req.body.done } };
+  const options = {};
+  await Todo.updateOne(query, update, options);
+  const updatedTodo = await Todo.findById(req.todo._id);
+  res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
