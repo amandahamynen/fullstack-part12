7d938c80b7427eadd1e951a8245cca6176cbf662
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-14 14:53:18 +0200

    exercises 12.9-12.11

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
@@ -14,5 +15,13 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  const addedTodos = await redis.getAsync('added_todos')
+
+  res.json({
+    added_todos: addedTodos ? Number(addedTodos) : 0
+  })
+})
 
 module.exports = router;
